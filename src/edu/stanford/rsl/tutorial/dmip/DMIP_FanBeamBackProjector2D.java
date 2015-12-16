c022b30442e0b2774278422e8859b3c941b1a5ef
Tristan Gottschalk <os73isad@stud.informatik.uni-erlangen.de>
2015-12-16 16:42:38 +0100

    FanBeam finished

--- src/edu/stanford/rsl/tutorial/dmip/DMIP_FanBeamBackProjector2D.java
+++ src/edu/stanford/rsl/tutorial/dmip/DMIP_FanBeamBackProjector2D.java
@@ -172,8 +172,8 @@ public class DMIP_FanBeamBackProjector2D {
 		//Create 1D kernel (identical for each projection)
 		Grid1D cosineKernel = new Grid1D(detectorPixels);
 		for(int i=0; i<detectorPixels; ++i){
-			//TODO
-			cosineKernel.setAtIndex(i, 9999);//TODO
+			double s = ( i + 0.5) * detectorSpacing - detectorLength/2;
+			cosineKernel.setAtIndex(i, (float) (focalLength / Math.sqrt(focalLength * focalLength + s * s)));
 		}
 		
 		//apply cosine weights to each projection
@@ -221,7 +221,9 @@ public class DMIP_FanBeamBackProjector2D {
 
 				// implement the conditions as described in Parker's paper
 				if (beta <= 2 * (delta - gamma)) {
-					float val = 0; //TODO
+					
+					double tmp = (Math.PI * beta) / (4.0 * (delta - gamma));
+					float val = (float) (Math.pow(Math.sin(tmp), 2));
 					if (Double.isNaN(val)){
 						continue;
 					}
@@ -231,7 +233,9 @@ public class DMIP_FanBeamBackProjector2D {
 					parker.setAtIndex(t, b , 1);
 				}
 				else if (beta <= (Math.PI + 2.d * delta) + 1e-12) {
-					float val = 0;//TODO
+					
+					double tmp = (Math.PI / 4.0) * ( (Math.PI + 2.0*delta - beta) / (delta + gamma));
+					float val = (float) (Math.pow(Math.sin(tmp), 2));
 					if (Double.isNaN(val)){
 						continue;
 					}
