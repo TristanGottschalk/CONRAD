e160137513b0fb44e610501eee182ef523486c40
Tristan Gottschalk <os73isad@stud.informatik.uni-erlangen.de>
2016-10-25 11:43:07 +0200

    Exercise 1 finished

--- src/edu/stanford/rsl/tutorial/praktikum/Phantom.java
+++ src/edu/stanford/rsl/tutorial/praktikum/Phantom.java
@@ -2,24 +2,21 @@ package edu.stanford.rsl.tutorial.praktikum;
 
 import ij.ImageJ;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.InterpolationOperators;
+import edu.stanford.rsl.conrad.data.numeric.NumericGrid;
 import edu.stanford.rsl.conrad.data.numeric.NumericPointwiseOperators;
 
 public class Phantom extends Grid2D {
-	public Phantom(int width, int height) {
+	public Phantom(int width, int height, double [] spacing) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
 		
-		// build square
-		int edgeLength = width/4;
-		int squareCenterX = width/2-edgeLength/6;
-		int squareCenterY = height/2-edgeLength/2;
-		for(int row = squareCenterY-edgeLength; 2 * row<squareCenterY+edgeLength; row++){
-			for (int col = squareCenterX-edgeLength; col < squareCenterX+edgeLength; col++){
-				this.setAtIndex(col,row, (.2f));
-			}
-		}
+		this.setSpacing(spacing);
+		double origin [] = new double [2];
+		origin[0] = -(width-1)*(spacing[0]/2);
+		origin[1] = -(height-1)*(spacing[1]/2);
+		this.setOrigin(origin);
 		
-		// circle
+		// circle for phantom
 		int circleCenterX = width/2+width/8;
 		int circleCenterY = height/2+height/10;
 		int radius = width/3;
@@ -31,26 +28,52 @@ public class Phantom extends Grid2D {
 			}
 		}
 		
-		// ellipse
+		// ellipse for phantom
 		int ellipseCenterX = width/4+width/8;
 		int ellipseCenterY = height/2-height/8;
-		int axisA = width/10;
-		int axisB = height/5;
+		int axisA = width/8;
+		int axisB = height/4;
 		for (int row = ellipseCenterY-axisB; row < ellipseCenterY+axisB; row++){
 			for (int col = ellipseCenterX-axisA; col < ellipseCenterX+axisA; col++) {
 				if (((col-ellipseCenterX)*(col-ellipseCenterX)*axisB*axisB)+((row-ellipseCenterY)*(row-ellipseCenterY)*axisA*axisA) <= axisA*axisA*axisB*axisB){
-					this.setAtIndex(col,row, .7f);
+					this.setAtIndex(col,row, .2f);
 				}
 			}
 		}
+		
+		// rectangle for phantom
+		int edgeLength = width/4;
+		int squareCenterX = width/2-edgeLength/6;
+		int squareCenterY = height/2-edgeLength/2;
+		for(int row = squareCenterY-edgeLength; 2 * row<squareCenterY+edgeLength; row++){
+			for (int col = squareCenterX-edgeLength; col < squareCenterX+edgeLength; col++){
+				this.setAtIndex(col,row, (.7f));
+			}
+		}
+		
 }
 
 	public static void main(String[] args) {
 		
 		new ImageJ();
-		Phantom a = new Phantom(512,512);
+		double spacing [] = {1 , 1};
+		Phantom a = new Phantom(256,256,spacing);
+		Phantom b = new Phantom(256,256,spacing);
+		
+		//show Phantoms
 		a.show();
-
+		b.show();
+		
+		//Subtract Phantoms
+		NumericGrid grid = NumericPointwiseOperators.subtractedBy(a, b);
+		grid.show();
+		
+		double origin [] = a.getOrigin();
+		System.out.println("Maximum intensity: " + NumericPointwiseOperators.min(a));
+		System.out.println("Minimum intensity: " + NumericPointwiseOperators.max(a));
+		System.out.println("Mean intensity: " + NumericPointwiseOperators.mean(a));
+		System.out.println("Position of origin: " + origin[0] + " " + origin[1]);
+		
 	}
 
 }
